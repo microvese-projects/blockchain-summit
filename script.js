8024994f9d9b615f0e2efef9e51da99e82289a9b
Benson Njuguna <njugunab655@gmail.com>
2023-03-14 09:24:40 +0300

    Fix linter errrors

--- script.js
+++ script.js
@@ -23,14 +23,13 @@ const speakersObj = [
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, optio. Quis voluptatum quaerat mollitia quae iste libero voluptates adipisci exercitationem?',
     image: 'images/speakers/vitalik_buterin.jpg',
   },
-]
+];
 const featuredSpeakersContainer = document.querySelector('#featured-speakers');
 
 function displaySpeaker({
   name, title, description, image,
 }) {
-  const speakerCard = 
-  `
+  const speakerCard = `
   <li class="speaker-card">
     <div class="images">
       <img src=${image} class="speaker-image" alt="Image of ${name}">
@@ -48,10 +47,8 @@ function displaySpeaker({
     </div>
   </li>
   
-  `
+  `;
   featuredSpeakersContainer.innerHTML += speakerCard;
 }
 
 speakersObj.forEach(displaySpeaker);
-
-
