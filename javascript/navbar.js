4c5c1bc7d7012328218c5158db27145c8bb77466
Benson Njuguna <njugunab655@gmail.com>
2023-03-14 18:33:29 +0300

    Style popup hamburger menu

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,7 +1,14 @@
 const hamburgerMenuBtn = document.querySelector(".hamburger-menu-icon > i");
+const closeHamburger = document.querySelector("#close-hamburger > i");
+const popupMenu = document.querySelector("#popup-menu");
 
 function showMenu(e) {
-  console.log(e.target)
+  popupMenu.classList.toggle("hide");
 }
 
-hamburgerMenuBtn.addEventListener('click', showMenu)
+function closeHamburgerMenu(e) {
+  popupMenu.classList.toggle("hide");
+}
+
+hamburgerMenuBtn.addEventListener('click', showMenu);
+closeHamburger.addEventListener("click", closeHamburgerMenu)
