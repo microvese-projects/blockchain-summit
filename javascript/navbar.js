0e938cfbe0b206fa9697488f04071906bb37ace7
Benson Njuguna <njugunab655@gmail.com>
2023-03-15 12:47:12 +0300

    Fix linters errors

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,14 +1,14 @@
-const hamburgerMenuBtn = document.querySelector(".hamburger-menu-icon > i");
-const closeHamburger = document.querySelector("#close-hamburger > i");
-const popupMenu = document.querySelector("#popup-menu");
+const hamburgerMenuBtn = document.querySelector('.hamburger-menu-icon > i');
+const closeHamburger = document.querySelector('#close-hamburger > i');
+const popupMenu = document.querySelector('#popup-menu');
 
-function showMenu(e) {
-  popupMenu.classList.toggle("hide");
+function showMenu() {
+  popupMenu.classList.toggle('hide');
 }
 
-function closeHamburgerMenu(e) {
-  popupMenu.classList.toggle("hide");
+function closeHamburgerMenu() {
+  popupMenu.classList.toggle('hide');
 }
 
 hamburgerMenuBtn.addEventListener('click', showMenu);
-closeHamburger.addEventListener("click", closeHamburgerMenu)
+closeHamburger.addEventListener('click', closeHamburgerMenu);
